bb46aa19833abf758e18dc29d587bc8c8ce4d6a9
Philipp Herz <rooki@rooki.xyz>
2023-11-13 20:20:29 +0100

    LINT

--- src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/privateMessage/controllers/PrivateMessageController.java
+++ src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/privateMessage/controllers/PrivateMessageController.java
@@ -16,7 +16,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
