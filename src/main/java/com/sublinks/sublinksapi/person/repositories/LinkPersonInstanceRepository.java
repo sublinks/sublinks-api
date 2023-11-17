453c6668bfc89dd0e1551e4934ae1e6847ec7fa6
Jason Grim <jason@gr.im>
2023-11-17 11:32:51 -0500

    Added isAdmin checks on site controller and fixed error handler for non-internal errors

--- src/main/java/com/sublinks/sublinksapi/person/repositories/LinkPersonInstanceRepository.java
+++ src/main/java/com/sublinks/sublinksapi/person/repositories/LinkPersonInstanceRepository.java
@@ -2,6 +2,7 @@ package com.sublinks.sublinksapi.person.repositories;
 
 import com.sublinks.sublinksapi.instance.dto.Instance;
 import com.sublinks.sublinksapi.person.dto.LinkPersonInstance;
+import com.sublinks.sublinksapi.person.dto.Person;
 import com.sublinks.sublinksapi.person.enums.LinkPersonInstanceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import java.util.Collection;
 
 public interface LinkPersonInstanceRepository extends JpaRepository<LinkPersonInstance, Long> {
     Collection<LinkPersonInstance> getLinkPersonInstancesByInstanceAndLinkTypeIsIn(Instance instance, Collection<LinkPersonInstanceType> linkTypes);
+
+    Collection<LinkPersonInstance> getLinkPersonInstancesByInstanceAndLinkTypeIsInAndPerson(Instance instance, Collection<LinkPersonInstanceType> linkTypes, Person person);
 }
