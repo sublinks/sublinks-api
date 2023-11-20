12aebcd489d3c7b4d09283e37f5bd4a180ae1192
Justin Kirk <thejuki@hotmail.com>
2023-11-19 18:59:31 -0600

    Finish adding swagger annotations

--- src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/user/controllers/UserModActionsController.java
+++ src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/user/controllers/UserModActionsController.java
@@ -4,9 +4,15 @@ import com.sublinks.sublinksapi.api.lemmy.v3.site.models.GetSiteResponse;
 import com.sublinks.sublinksapi.api.lemmy.v3.user.models.BanPersonResponse;
 import com.sublinks.sublinksapi.api.lemmy.v3.user.models.BlockPersonResponse;
 import com.sublinks.sublinksapi.api.lemmy.v3.user.models.GetReportCountResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,26 +22,50 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v3/user")
-@Tag(name = "user", description = "the user API")
+@Tag(name = "User")
 public class UserModActionsController {
+    @Operation(summary = "Ban a person from your site.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BanPersonResponse.class))})
+    })
     @PostMapping("ban")
     BanPersonResponse ban() {
 
         return BanPersonResponse.builder().build();
     }
 
+    @Operation(summary = "Block a person.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BlockPersonResponse.class))})
+    })
     @PostMapping("block")
     BlockPersonResponse block() {
 
         return BlockPersonResponse.builder().build();
     }
 
+    @Operation(summary = "Get counts for your reports.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = GetReportCountResponse.class))})
+    })
     @GetMapping("report_count")
     GetReportCountResponse reportCount() {
 
         return GetReportCountResponse.builder().build();
     }
 
+    @Operation(summary = "Leave the Site admins.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = GetSiteResponse.class))})
+    })
     @PostMapping("leave_admin")
     GetSiteResponse leaveAdmin() {
 
