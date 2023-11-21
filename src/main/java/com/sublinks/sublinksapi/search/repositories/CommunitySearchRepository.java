b80133f3e0d4de342ecd81ee8103d740769f3805
Philipp Herz <rooki@rooki.xyz>
2023-11-21 22:17:36 +0100

    Implemented the rest.

--- src/main/java/com/sublinks/sublinksapi/search/repositories/CommunitySearchRepository.java
+++ src/main/java/com/sublinks/sublinksapi/search/repositories/CommunitySearchRepository.java
@@ -11,7 +11,8 @@ public interface CommunitySearchRepository extends JpaRepository<Community, Long
 
    // @Query(value = "SELECT p.*, ppc.* FROM posts p LEFT JOIN post_post_cross_post ppc ON ppc.post_id = p.id WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(p.post_body) LIKE LOWER(CONCAT('%', :keyword,'%'))",
    //        nativeQuery = true)
-   @Query(value = "SELECT c.*, cl.* FROM communities c LEFT JOIN community_languages cl on cl.language_id = communitiy_id WHERE MATCH(c.title, c.titleSlug, c.description) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+   @Query(value = "SELECT c.* FROM communities c WHERE MATCH(c.title, c.title_slug, c.description) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+           countQuery = "SELECT COUNT(c.id) FROM communities c WHERE MATCH(c.title, c.title_slug, c.description) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
            nativeQuery = true)
     Page<Community> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
