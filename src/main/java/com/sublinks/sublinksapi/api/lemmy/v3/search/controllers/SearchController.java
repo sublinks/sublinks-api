3db1108a35dcbd77f3e9960c3daa1b00db87720e
Philipp Herz <rooki@rooki.xyz>
2023-11-21 22:28:58 +0100

    Changed the limit to 20

--- src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/search/controllers/SearchController.java
+++ src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/search/controllers/SearchController.java
@@ -6,7 +6,7 @@ import com.sublinks.sublinksapi.api.lemmy.v3.common.controllers.AbstractLemmyApi
 import com.sublinks.sublinksapi.api.lemmy.v3.community.models.CommunityView;
 import com.sublinks.sublinksapi.api.lemmy.v3.community.services.LemmyCommunityService;
 import com.sublinks.sublinksapi.api.lemmy.v3.enums.SearchType;
-import com.sublinks.sublinksapi.api.lemmy.v3.enums.SortType;
+import com.sublinks.sublinksapi.api.lemmy.v3.post.models.PostResponse;
 import com.sublinks.sublinksapi.api.lemmy.v3.post.models.PostView;
 import com.sublinks.sublinksapi.api.lemmy.v3.post.services.LemmyPostService;
 import com.sublinks.sublinksapi.api.lemmy.v3.search.models.Search;
@@ -18,86 +18,106 @@ import com.sublinks.sublinksapi.community.dto.Community;
 import com.sublinks.sublinksapi.person.dto.Person;
 import com.sublinks.sublinksapi.post.dto.Post;
 import com.sublinks.sublinksapi.search.services.SearchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v3/search")
+@Tag(name = "Miscellaneous")
 public class SearchController extends AbstractLemmyApiController {
-    private final SearchService searchService;
-    private final LemmyCommunityService lemmyCommunityService;
-    private final LemmyCommentService lemmyCommentService;
-    private final LemmyPostService lemmyPostService;
-    private final LemmyPersonService lemmyPersonService;
 
-    @GetMapping
-    SearchResponse search(@Valid final Search searchForm) {
+  private final SearchService searchService;
+  private final LemmyCommunityService lemmyCommunityService;
+  private final LemmyCommentService lemmyCommentService;
+  private final LemmyPostService lemmyPostService;
+  private final LemmyPersonService lemmyPersonService;
 
-        final SearchResponse.SearchResponseBuilder responseBuilder = SearchResponse.builder();
+  @Operation(summary = "Search lemmy.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK",
+          content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+              schema = @Schema(implementation = PostResponse.class))})
+  })
+  @GetMapping
+  SearchResponse search(@Valid final Search searchForm) {
 
-        final int page = searchForm.page() == null ? 0 : Integer.parseInt(searchForm.page());
-        final int pLimit = Integer.parseInt(searchForm.limit());
-        final int limit = Math.min(pLimit, 25);
-        if(searchForm.type_() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+    final SearchResponse.SearchResponseBuilder responseBuilder = SearchResponse.builder();
 
-        Sort sort = switch (searchForm.sort()) {
-            case "New" -> Sort.by("createdAt").descending();
-            case "Old" -> Sort.by("createdAt").ascending();
-            default -> Sort.unsorted();
-        };
-
-        final boolean isAll = searchForm.type_() == SearchType.All;
-
-        if(searchForm.type_() == SearchType.Comments || isAll)
-        {
-            List<Comment> comments = searchService.searchComments(searchForm.q(), page, limit, sort).getContent();
-            List<CommentView> commentViewList = comments.stream().map(lemmyCommentService::createCommentView).toList();
-            
-            responseBuilder.comments(commentViewList);
-        }
-        if(searchForm.type_() == SearchType.Posts || isAll)
-        {
-            List<Post> posts = searchService.searchPost(searchForm.q(), page, limit, sort).getContent();
-            List<PostView> commentViewList = posts.stream().map(lemmyPostService::postViewFromPost).toList();
-
-            responseBuilder.posts(commentViewList);
-        }
-        if(searchForm.type_() == SearchType.Communities || isAll)
-        {
-            List<Community> communities = searchService.searchCommunity(searchForm.q(), page, limit, sort).getContent();
-            List<CommunityView> commentViewList = communities.stream().map(lemmyCommunityService::communityViewFromCommunity).toList();
-
-            responseBuilder.communities(commentViewList);
-        }
-        if(searchForm.type_() == SearchType.Users || isAll)
-        {
-            List<Person> people = searchService.seearchPerson(searchForm.q(), page, limit, sort).getContent();
-            List<PersonView> peopleViewList = people.stream().map(lemmyPersonService::getPersonView).toList();
-
-            responseBuilder.users(peopleViewList);
-        }
-
-        if(searchForm.type_() == SearchType.Url || isAll)
-        {
-            List<Post> posts = searchService.searchPostByUrl(searchForm.q(), page, limit, sort).getContent();
-            List<PostView> commentViewList = posts.stream().map(lemmyPostService::postViewFromPost).toList();
-
-            responseBuilder.posts(commentViewList);
-        }
-
-        return responseBuilder.build();
+    final int page = (searchForm.page() == null ? 0 : Integer.parseInt(searchForm.page())) - 1;
+    final int pLimit = Integer.parseInt(searchForm.limit());
+    final int limit = Math.min(pLimit, 20);
+    if (searchForm.type_() == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
     }
+
+    Sort sort = switch (searchForm.sort()) {
+      case "New" -> Sort.by("createdAt").descending();
+      case "Old" -> Sort.by("createdAt").ascending();
+      default -> Sort.unsorted();
+    };
+
+    final boolean isAll = searchForm.type_() == SearchType.All;
+
+    final List<CommentView> commentViewList = new ArrayList<>();
+    final List<PostView> postViewList = new ArrayList<>();
+    final List<CommunityView> communityViewList = new ArrayList<>();
+    final List<PersonView> peopleViewList = new ArrayList<>();
+
+    if (searchForm.type_() == SearchType.Comments || isAll) {
+      List<Comment> comments = searchService.searchComments(searchForm.q(), page, limit, sort)
+          .getContent();
+      commentViewList.addAll(
+          comments.stream().map(lemmyCommentService::createCommentView).toList());
+
+    }
+
+    if (searchForm.type_() == SearchType.Posts || isAll) {
+      List<Post> posts = searchService.searchPost(searchForm.q(), page, limit, sort).getContent();
+      postViewList.addAll(posts.stream().map(lemmyPostService::postViewFromPost).toList());
+    }
+
+    if (searchForm.type_() == SearchType.Communities || isAll) {
+      List<Community> communities = searchService.searchCommunity(searchForm.q(), page, limit, sort)
+          .getContent();
+      communityViewList.addAll(
+          communities.stream().map(lemmyCommunityService::communityViewFromCommunity).toList());
+
+    }
+
+    if (searchForm.type_() == SearchType.Users || isAll) {
+      List<Person> people = searchService.seearchPerson(searchForm.q(), page, limit, sort)
+          .getContent();
+      peopleViewList.addAll(people.stream().map(lemmyPersonService::getPersonView).toList());
+
+    }
+
+    if (searchForm.type_() == SearchType.Url || isAll) {
+      List<Post> posts = searchService.searchPostByUrl(searchForm.q(), page, limit, sort)
+          .getContent();
+      postViewList.addAll(posts.stream().map(lemmyPostService::postViewFromPost).toList());
+    }
+
+    responseBuilder.comments(commentViewList);
+    responseBuilder.posts(postViewList);
+    responseBuilder.communities(communityViewList);
+    responseBuilder.users(peopleViewList);
+
+    return responseBuilder.build();
+  }
 }
