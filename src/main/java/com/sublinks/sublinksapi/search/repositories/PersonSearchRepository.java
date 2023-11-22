9f08ca2f832995585df4bfc9f323823c5c8ade70
Philipp Herz <rooki@rooki.xyz>
2023-11-22 15:52:43 +0100

    Improved the count queries.

--- src/main/java/com/sublinks/sublinksapi/search/repositories/PersonSearchRepository.java
+++ src/main/java/com/sublinks/sublinksapi/search/repositories/PersonSearchRepository.java
@@ -11,6 +11,7 @@ import org.springframework.data.repository.query.Param;
 public interface PersonSearchRepository extends JpaRepository<Person, Long> {
 
    @Query(value = "SELECT p.*, lpi.instance_id FROM people p LEFT JOIN link_person_instances lpi ON lpi.person_id = p.id WHERE MATCH(p.name, p.display_name) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+           countQuery = "SELECT COUNT(p.id) FROM people p WHERE MATCH(p.name, p.display_name) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
            nativeQuery = true)
     Page<Person> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
