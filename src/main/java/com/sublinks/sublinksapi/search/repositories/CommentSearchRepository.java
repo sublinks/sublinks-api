7d01fec7f9feaa5bc9a77d33761cd06137ad10c1
Philipp Herz <rooki@rooki.xyz>
2023-11-23 20:05:57 +0100

    Resolved every pr request

--- src/main/java/com/sublinks/sublinksapi/search/repositories/CommentSearchRepository.java
+++ src/main/java/com/sublinks/sublinksapi/search/repositories/CommentSearchRepository.java
@@ -10,10 +10,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface CommentSearchRepository extends JpaRepository<Comment, Long> {
 
-   // @Query(value = "SELECT p.*, ppc.* FROM posts p LEFT JOIN post_post_cross_post ppc ON ppc.post_id = p.id WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(p.post_body) LIKE LOWER(CONCAT('%', :keyword,'%'))",
-   //        nativeQuery = true)
-   @Query(value = "SELECT c.* FROM comments c LEFT JOIN posts p on p.id = c.post_id LEFT JOIN people pp ON pp.id = c.person_id LEFT JOIN communities cm ON cm.id = c.community_id WHERE MATCH(c.comment_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
-           countQuery = "SELECT COUNT(c.id) FROM comments c WHERE MATCH(c.comment_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
-           nativeQuery = true)
-    Page<Comment> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
+  @Query(value = "SELECT c.* FROM comments c LEFT JOIN posts p on p.id = c.post_id LEFT JOIN people pp ON pp.id = c.person_id LEFT JOIN communities cm ON cm.id = c.community_id WHERE MATCH(c.comment_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+      countQuery = "SELECT COUNT(c.id) FROM comments c WHERE MATCH(c.comment_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+      nativeQuery = true)
+  Page<Comment> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
