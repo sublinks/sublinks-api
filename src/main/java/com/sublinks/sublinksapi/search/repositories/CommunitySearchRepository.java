1ab8e7adfb5c38b90b3bfb6eb6635a1391dd6064
rooki <rooki@rooki.xyz>
2023-11-23 20:05:57 +0100

    Resolved every pr request

--- src/main/java/com/sublinks/sublinksapi/search/repositories/CommunitySearchRepository.java
+++ src/main/java/com/sublinks/sublinksapi/search/repositories/CommunitySearchRepository.java
@@ -9,10 +9,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface CommunitySearchRepository extends JpaRepository<Community, Long> {
 
-   // @Query(value = "SELECT p.*, ppc.* FROM posts p LEFT JOIN post_post_cross_post ppc ON ppc.post_id = p.id WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(p.post_body) LIKE LOWER(CONCAT('%', :keyword,'%'))",
-   //        nativeQuery = true)
-   @Query(value = "SELECT c.* FROM communities c WHERE MATCH(c.title, c.title_slug, c.description) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
-           countQuery = "SELECT COUNT(c.id) FROM communities c WHERE MATCH(c.title, c.title_slug, c.description) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
-           nativeQuery = true)
-    Page<Community> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
+  @Query(value = "SELECT c.* FROM communities c WHERE MATCH(c.title, c.title_slug, c.description) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+      countQuery = "SELECT COUNT(c.id) FROM communities c WHERE MATCH(c.title, c.title_slug, c.description) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+      nativeQuery = true)
+  Page<Community> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
--- src/main/java/com/sublinks/sublinksapi/search/repositories/PostSearchRepository.java
+++ src/main/java/com/sublinks/sublinksapi/search/repositories/PostSearchRepository.java
@@ -9,10 +9,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface PostSearchRepository extends JpaRepository<Post, Long> {
 
-   // @Query(value = "SELECT p.*, ppc.* FROM posts p LEFT JOIN post_post_cross_post ppc ON ppc.post_id = p.id WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(p.post_body) LIKE LOWER(CONCAT('%', :keyword,'%'))",
-   //        nativeQuery = true)
-   @Query(value = "SELECT p.*, ppc.* FROM posts p LEFT JOIN post_post_cross_post ppc ON ppc.post_id = p.id WHERE MATCH(p.title, p.post_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
-           countQuery = "SELECT COUNT(p.id) FROM posts p WHERE MATCH(p.title, p.post_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
-           nativeQuery = true)
-    Page<Post> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
+  @Query(value = "SELECT p.*, ppc.* FROM posts p LEFT JOIN post_post_cross_post ppc ON ppc.post_id = p.id WHERE MATCH(p.title, p.post_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+      countQuery = "SELECT COUNT(p.id) FROM posts p WHERE MATCH(p.title, p.post_body) AGAINST (CONCAT('*', :keyword, '*') IN BOOLEAN MODE);",
+      nativeQuery = true)
+  Page<Post> searchAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
