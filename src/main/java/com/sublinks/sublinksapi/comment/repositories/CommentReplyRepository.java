10ae91becbe7046396fc6f47e3f457d628f072a1
rooki <rooki@rooki.xyz>
2023-11-25 13:03:54 +0100

    Implemented unread count

--- src/main/java/com/sublinks/sublinksapi/comment/repositories/CommentReplyRepository.java
+++ src/main/java/com/sublinks/sublinksapi/comment/repositories/CommentReplyRepository.java
@@ -1,10 +1,13 @@
 package com.sublinks.sublinksapi.comment.repositories;
 
 import com.sublinks.sublinksapi.comment.dto.CommentReply;
+import com.sublinks.sublinksapi.person.dto.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CommentReplyRepository extends JpaRepository<CommentReply, Long>, CommentReplyRepositorySearch {
+public interface CommentReplyRepository extends JpaRepository<CommentReply, Long>,
+    CommentReplyRepositorySearch {
 
+  long countByRecipientAndIsReadFalse(Person recipient);
 }
--- src/main/java/com/sublinks/sublinksapi/privatemessages/repositories/PrivateMessageRepository.java
+++ src/main/java/com/sublinks/sublinksapi/privatemessages/repositories/PrivateMessageRepository.java
@@ -1,9 +1,12 @@
 package com.sublinks.sublinksapi.privatemessages.repositories;
 
+import com.sublinks.sublinksapi.person.dto.Person;
 import com.sublinks.sublinksapi.privatemessages.dto.PrivateMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PrivateMessageRepository extends JpaRepository<PrivateMessage, Long>,
     PrivateMessageRepositorySearch {
 
+  long countByRecipientAndIsReadFalse(Person recipient);
 }
