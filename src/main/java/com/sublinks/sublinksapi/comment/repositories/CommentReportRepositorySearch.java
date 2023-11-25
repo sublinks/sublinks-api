b9157cca770e7e7f7f4ebcd5da4cd5336710f851
Philipp Herz <rooki@rooki.xyz>
2023-11-25 11:49:11 +0100

    Implemented reports_count

--- src/main/java/com/sublinks/sublinksapi/comment/repositories/CommentReportRepositorySearch.java
+++ src/main/java/com/sublinks/sublinksapi/comment/repositories/CommentReportRepositorySearch.java
@@ -1,12 +1,17 @@
 package com.sublinks.sublinksapi.comment.repositories;
 
-import com.sublinks.sublinksapi.comment.dto.Comment;
 import com.sublinks.sublinksapi.comment.dto.CommentReport;
 import com.sublinks.sublinksapi.comment.models.CommentReportSearchCriteria;
-import com.sublinks.sublinksapi.comment.models.CommentSearchCriteria;
+import com.sublinks.sublinksapi.community.dto.Community;
+import jakarta.annotation.Nullable;
 import java.util.List;
 
 public interface CommentReportRepositorySearch {
 
-  List<CommentReport> allCommentReportsBySearchCriteria(CommentReportSearchCriteria commentReportSearchCriteria);
+  List<CommentReport> allCommentReportsBySearchCriteria(
+      CommentReportSearchCriteria commentReportSearchCriteria);
+
+  long countAllCommentReportsByResolvedFalseAndCommunity(@Nullable Community community);
+
+  long countAllCommentReportsByResolvedFalse();
 }
