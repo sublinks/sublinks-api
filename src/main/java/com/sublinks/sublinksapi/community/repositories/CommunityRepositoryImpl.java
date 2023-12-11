3c34dc899f725a9ec110718e5f543fcdd114096b
Philipp Herz <rooki@rooki.xyz>
2023-12-11 10:41:06 +0100

    Add community search criteria for list API
    This commit introduces a new model, CommunitySearchCriteria that is used to filter out communities by specific criteria in the list API. It also includes the modification of CommunityRepository to use this new search criteria and the implementation of these changes in CommunityController. For the new search criteria, a new file, CommunitySearchRepository, was added to provide more flexible search options. Lastly, some code formatting and clean up has been done for better readability.

---
+++ src/main/java/com/sublinks/sublinksapi/community/repositories/CommunityRepositoryImpl.java
@@ -0,0 +1,81 @@
+package com.sublinks.sublinksapi.community.repositories;
+
+import static com.sublinks.sublinksapi.utils.PaginationUtils.applyPagination;
+
+import com.sublinks.sublinksapi.community.dto.Community;
+import com.sublinks.sublinksapi.community.models.CommunitySearchCriteria;
+import com.sublinks.sublinksapi.person.enums.LinkPersonCommunityType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CommunityRepositoryImpl implements CommunitySearchRepository {
+
+  @Autowired
+  EntityManager em;
+
+  @Override
+  public List<Community> allCommunitiesBySearchCriteria(
+      final CommunitySearchCriteria communitySearchCriteria) {
+
+    final CriteriaBuilder cb = em.getCriteriaBuilder();
+    final CriteriaQuery<Community> cq = cb.createQuery(Community.class);
+
+    final Root<Community> communityTable = cq.from(Community.class);
+
+    final List<Predicate> predicates = new ArrayList<>();
+
+    if (!communitySearchCriteria.showNsfw()) {
+      predicates.add(cb.equal(communityTable.get("isNsfw"), false));
+    }
+
+    switch (communitySearchCriteria.listingType()) {
+      case Subscribed:
+        if (communitySearchCriteria.person() == null) {
+          break;
+        }
+        final var linkPersonCommunityJoin = communityTable.join("linkPersonCommunity");
+        final List<Predicate> subPredicates = new ArrayList<>();
+        subPredicates.add(
+            cb.equal(linkPersonCommunityJoin.get("person"), communitySearchCriteria.person()));
+        subPredicates.add(
+            cb.equal(linkPersonCommunityJoin.get("linkType"), LinkPersonCommunityType.follower));
+
+        predicates.add(cb.and(subPredicates.toArray(new Predicate[0])));
+        break;
+      case Local:
+        predicates.add(cb.equal(communityTable.get("isLocal"), true));
+        break;
+      default:
+        break;
+    }
+
+    cq.where(predicates.toArray(new Predicate[0]));
+
+    switch (communitySearchCriteria.sortType()) {
+      case New:
+        cq.orderBy(cb.desc(communityTable.get("createdAt")));
+        break;
+      case Old:
+        cq.orderBy(cb.asc(communityTable.get("createdAt")));
+        break;
+      default:
+        cq.orderBy(cb.desc(communityTable.get("createdAt")));
+        break;
+    }
+
+    int perPage = Math.min(Math.abs(communitySearchCriteria.perPage()), 20);
+
+    TypedQuery<Community> query = em.createQuery(cq);
+
+    applyPagination(query, communitySearchCriteria.page(), perPage);
+
+    return query.getResultList();
+  }
+}
