a10bc7c5292ad516358a4caeef19e7c28f1862f9
Carson Weeks <mail@carsonweeks.com>
2023-12-22 17:39:36 -0800

    Added JavaDocs to SlugUtil

--- src/main/java/com/sublinks/sublinksapi/utils/SlugUtil.java
+++ src/main/java/com/sublinks/sublinksapi/utils/SlugUtil.java
@@ -6,6 +6,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class SlugUtil {
 
+  /**
+   * Converts a given string to a slug format.
+   * This method transforms the input string to lowercase, replaces new lines and
+   * non-alphanumeric characters
+   * with spaces, and then collapses multiple spaces into a single underscore.
+   *
+   * @param title The string to be converted into slug format.
+   * @return A slug version of the provided string.
+   */
   public String stringToSlug(String title) {
 
     return title
@@ -15,11 +24,26 @@ public class SlugUtil {
         .replaceAll("\\s+", "_");
   }
 
+  /**
+   * Generates a unique slug for a given title.
+   * This method combines a randomly generated string with a slug version of the
+   * provided title,
+   * separated by a hyphen. The random string ensures uniqueness of the slug.
+   *
+   * @param title The title for which a unique slug is to be generated.
+   * @return A unique slug for the given title.
+   */
   public String uniqueSlug(String title) {
 
     return randomStringGenerator() + "-" + stringToSlug(title);
   }
 
+  /**
+   * Generates a random string of lowercase alphabetic characters.
+   * This method creates a random string of 8 characters in the range 'a' to 'z'.
+   *
+   * @return A random string of 8 lowercase letters.
+   */
   public String randomStringGenerator() {
 
     final Random random = new Random();
