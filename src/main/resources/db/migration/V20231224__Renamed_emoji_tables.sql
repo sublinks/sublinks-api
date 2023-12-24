a6af88f2bc2a082dd5b64d65b629de1a6c72b530
Carson Weeks <mail@carsonweeks.com>
2023-12-24 12:59:17 -0800

    Updated model names to plural

---
+++ src/main/resources/db/migration/V20231224__Renamed_emoji_tables.sql
@@ -0,0 +1,9 @@
+/**
+  Custom Emoji table
+ */
+RENAME TABLE `custom_emoji` TO `custom_emojis`;
+
+/**
+  Custom Emoji Keyword table
+ */
+RENAME TABLE `custom_emoji_keyword` TO `custom_emoji_keywords`;
