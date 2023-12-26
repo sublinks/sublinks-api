9beb927c36ed523ec0baef302393ebd56d72584a
Carson Weeks <mail@carsonweeks.com>
2023-12-25 18:51:06 -0800

    Added equals and hashcode to Custom Emoji Keyword.

--- src/main/java/com/sublinks/sublinksapi/customemoji/dto/CustomEmojiKeyword.java
+++ src/main/java/com/sublinks/sublinksapi/customemoji/dto/CustomEmojiKeyword.java
@@ -5,6 +5,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+import org.hibernate.proxy.HibernateProxy;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -39,5 +44,31 @@ public class CustomEmojiKeyword {
 
   @Column(nullable = false, name = "keyword")
   private String keyword;
+
+  @Override
+  public final boolean equals(Object o) {
+
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    Class<?> objectEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer()
+        .getPersistentClass() : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+        .getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != objectEffectiveClass) {
+      return false;
+    }
+    var customEmojiKeyword = (CustomEmojiKeyword) o;
+    return getId() != null && Objects.equals(getId(), customEmojiKeyword.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+        .getPersistentClass().hashCode() : getClass().hashCode();
+  }
 
 }
