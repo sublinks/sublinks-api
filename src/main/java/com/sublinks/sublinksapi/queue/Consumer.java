8c5e142f00d22317e3d742aa2216fc40fdad8455
Devan <devandbenz@gmail.com>
2024-01-20 16:52:49 -0600

    adding some more base functionality

---
+++ src/main/java/com/sublinks/sublinksapi/queue/Consumer.java
@@ -0,0 +1,13 @@
+package com.sublinks.sublinksapi.queue;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+  @RabbitListener(queues = "${FEDERATION_QUEUE_NAME}")
+  public void receive(String msg) {
+    // TODO: add message processing
+  }
+}
---
+++ src/test/java/com/sublinks/sublinksapi/queue/ConsumerTest.java
@@ -0,0 +1,30 @@
+package com.sublinks.sublinksapi.queue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import static org.mockito.Mockito.verify;
+
+public class ConsumerTest {
+  @InjectMocks
+  private Consumer consumer;
+
+  @BeforeEach
+  public void setUp() {
+
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void whenReceiveMessage_processMessage() {
+    String msg = "mock-msg";
+
+    consumer.receive(msg);
+
+    // TODO: add test for message processing
+  }
+}
