e167cb5d83ee481c1bef71ce9c22e57932838192
Devan <devandbenz@gmail.com>
2024-01-21 14:57:19 -0600

    rename env, add json message support

--- src/main/java/com/sublinks/sublinksapi/queue/QueueConfig.java
+++ src/main/java/com/sublinks/sublinksapi/queue/QueueConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,18 @@ public class QueueConfig {
 
   @Value("${FEDERATION_TOPIC_NAME}")
   private String federationTopicName;
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+    final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
 
   @Bean
   public Queue federationQueue() {
