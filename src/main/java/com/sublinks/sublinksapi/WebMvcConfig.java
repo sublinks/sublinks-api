b67298cf38c3ecb03b62a4dc6dd1a0387a2fcf6e
Devan <devandbenz@gmail.com>
2024-01-24 08:48:57 -0600

    start adding middleware

--- src/main/java/com/sublinks/sublinksapi/WebMvcConfig.java
+++ src/main/java/com/sublinks/sublinksapi/WebMvcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry){
-    registry.addInterceptor(new RateLimiter());
+    registry.addInterceptor(new RateLimiter())
+        .excludePathPatterns("/api/v3/site");
   }
 }
--- src/main/java/com/sublinks/sublinksapi/interceptors/RateLimiter.java
+++ src/main/java/com/sublinks/sublinksapi/interceptors/RateLimiter.java
@@ -1,12 +1,22 @@
 package com.sublinks.sublinksapi.interceptors;
 
+import com.sublinks.sublinksapi.person.dto.Person;
+import com.sublinks.sublinksapi.person.services.PersonService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class RateLimiter implements HandlerInterceptor {
+  final private PersonService personService;
+
+  @Autowired
+  public RateLimiter(PersonService personService) {
+    this.personService = personService;
+  }
+
   @Override
   public boolean preHandle(
       HttpServletRequest request,
