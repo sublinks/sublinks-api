da1965fe340fc25b714af56ac4fa375652e1fa6a
rooki <34943569+Pdzly@users.noreply.github.com>
2024-01-25 21:59:45 +0100

    Implemented Import and Export

---
+++ src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/user/models/SuccessResponse.java
@@ -0,0 +1,12 @@
+package com.sublinks.sublinksapi.api.lemmy.v3.user.models;
+
+import lombok.Builder;
+
+@Builder
+@SuppressWarnings("RecordComponentName")
+public record SuccessResponse(
+    Boolean success
+) {
+
+
+}
