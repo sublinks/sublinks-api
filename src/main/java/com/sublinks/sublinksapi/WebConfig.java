6ea59d9af9bd15bbb8758d1ec1e449c241bd4f37
Devan <devandbenz@gmail.com>
2024-01-26 13:14:38 -0600

    feat: rname

--- src/main/java/com/sublinks/sublinksapi/WebConfig.java
+++ src/main/java/com/sublinks/sublinksapi/WebConfig.java
@@ -1,16 +1,15 @@
 package com.sublinks.sublinksapi;
 
 import com.sublinks.sublinksapi.interceptors.RateLimiter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
   private final RateLimiter rateLimiter;
 
-  public WebMvcConfig(RateLimiter rateLimiter) {
+  public WebConfig(RateLimiter rateLimiter) {
     this.rateLimiter = rateLimiter;
   }
 
