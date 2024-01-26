65201c7e606c5519ec45c8f1c35d6f2d44c899b1
Devan <devandbenz@gmail.com>
2024-01-26 08:41:10 -0600

    feat: updates

--- src/main/java/com/sublinks/sublinksapi/interceptors/RateLimiter.java
+++ src/main/java/com/sublinks/sublinksapi/interceptors/RateLimiter.java
@@ -1,25 +1,28 @@
 package com.sublinks.sublinksapi.interceptors;
 
-import com.sublinks.sublinksapi.api.lemmy.v3.authentication.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
-import reactor.core.scheduler.Schedulers;
+import reactor.core.Disposable;
+import reactor.core.publisher.Mono;
 
-import java.io.IOException;
 import java.time.Duration;
 
 @Component
 public class RateLimiter implements HandlerInterceptor {
 
-  private final ReactiveRedisTemplate<String, String> redis;
+  @Value("${LIMITER_MAX_REQS:10}")
+  private int MAX_REQS;
+  @Value("${LIMITER_DURATION:30}")
+  private int LIMITER_DURATION;
   private String userName;
-  private final int MAX_REQS = 5;
-  private final int LIMITER_DURATION = 10;
+  private final ReactiveRedisTemplate<String, String> redis;
   private final int TOO_MANY_REQUESTS = 429;
 
   @Autowired
@@ -32,27 +35,30 @@ public class RateLimiter implements HandlerInterceptor {
       HttpServletRequest request,
       HttpServletResponse response,
       Object handler
-  ) {
+  ) throws ResponseStatusException {
     try {
       userName = request.getUserPrincipal().getName();
-    } catch (Exception e) {}
+      // request.getUserPrincipal() will throw if user not authed. We will ignore and just
+      // set remote address as key for rate-limiting.
+    } catch (Exception ignored) {}
 
     String key = userName != null ? userName : request.getRemoteAddr();
 
     try {
       Long result = this.redis.opsForValue().increment(key).block();
 
-      this.redis.expire(key, Duration.ofSeconds(LIMITER_DURATION)).subscribe();
-
       if (result != null && result > MAX_REQS) {
+        Duration ttl = this.redis.getExpire(key).block();
         response.setStatus(TOO_MANY_REQUESTS);
+          assert ttl != null;
+          response.setHeader("Retry-After", String.valueOf(ttl.getSeconds()));
         return false;
       }
 
+      this.redis.expire(key, Duration.ofSeconds(LIMITER_DURATION)).subscribe();
       return true;
     } catch (Exception e) {
-
-      return true;
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "interceptor_error");
     }
   }
 }
