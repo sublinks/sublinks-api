75890f0baac11015f6c7b3c29da5a124f7e9fb4e
Devan <devandbenz@gmail.com>
2024-01-27 17:03:36 -0600

    feat: adds ability for optional rabbitMQ usage for federation

--- src/main/java/com/sublinks/sublinksapi/queue/services/Consumer.java
+++ src/main/java/com/sublinks/sublinksapi/queue/services/Consumer.java
@@ -1,8 +1,11 @@
 package com.sublinks.sublinksapi.queue.services;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnBean(RabbitTemplate.class)
 public class Consumer {
 
   /*@RabbitListener(queues = "${FEDERATION_QUEUE_NAME}")
