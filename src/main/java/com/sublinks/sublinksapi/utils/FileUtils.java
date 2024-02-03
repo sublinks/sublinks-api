43836e38a23a3d7cf205f5685eceb3864d72adbb
rooki <34943569+Pdzly@users.noreply.github.com>
2024-02-03 15:54:59 +0100

    Add email functionality and templates
    This commit introduces an email functionality to the application. New email templates for various events (e.g., registration, password reset) were added. The necessary configurations were set up to allow sending emails and formatting the email templates. Relevant listeners and services were implemented to handle emailing logic when these events occur. Some related changes were also made in user registration logic.

---
+++ src/main/java/com/sublinks/sublinksapi/utils/FileUtils.java
@@ -0,0 +1,23 @@
+package com.sublinks.sublinksapi.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileUtils {
+
+  public static String readFromInputStream(InputStream inputStream)
+      throws IOException {
+
+    StringBuilder resultStringBuilder = new StringBuilder();
+    try (BufferedReader br
+        = new BufferedReader(new InputStreamReader(inputStream))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        resultStringBuilder.append(line).append("\n");
+      }
+    }
+    return resultStringBuilder.toString();
+  }
+}
