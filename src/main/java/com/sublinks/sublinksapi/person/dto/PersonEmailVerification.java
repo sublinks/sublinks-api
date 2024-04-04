2268bf7998b387bee065bb8de3da73fee39ac26a
rooki <34943569+Pdzly@users.noreply.github.com>
2024-04-04 19:42:02 +0200

    Implemented email verification feature
    This commit introduces a fully fledged email verification feature. It adds a new table `person_email_verification` to the database to store email verification tokens. It also updates `UserAuthController` to handle email verification logic. Furthermore, new events and services were added to manage the creation and updation of email verification details. This commit also ensures backward compatibility by modifying existing migrations to create foreign key references correctly.

---
+++ src/main/java/com/sublinks/sublinksapi/person/dto/PersonEmailVerification.java
@@ -0,0 +1,51 @@
+package com.sublinks.sublinksapi.person.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "person_email_verification")
+public class PersonEmailVerification {
+
+  /**
+   * Relationships.
+   */
+  @ManyToOne
+  @JoinColumn(name = "person_id")
+  private Person person;
+
+  /**
+   * Attributes.
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, name = "ip_address")
+  private String ipAddress;
+
+  @Column(nullable = true, name = "user_agent")
+  private String userAgent;
+
+  @Column(nullable = false, name = "token")
+  private String token;
+
+  @Column(nullable = false, name = "active")
+  private boolean active;
+
+  @CreationTimestamp
+  @Column(updatable = false, nullable = false, name = "created_at")
+  private Date createdAt;
+
+  @UpdateTimestamp
+  @Column(updatable = true, nullable = false, name = "updated_at")
+  private Date updatedAt;
+}
