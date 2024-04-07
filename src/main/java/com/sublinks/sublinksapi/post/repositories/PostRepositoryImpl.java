b394e58fcd545a211c64cab81121860faaf32243
Jason Grim <jason@gr.im>
2024-04-06 23:45:04 -0400

    WIP Cursor pagination

--- src/main/java/com/sublinks/sublinksapi/post/repositories/PostRepositoryImpl.java
+++ src/main/java/com/sublinks/sublinksapi/post/repositories/PostRepositoryImpl.java
@@ -1,22 +1,18 @@
 package com.sublinks.sublinksapi.post.repositories;
 
-import static com.sublinks.sublinksapi.utils.PaginationUtils.applyPagination;
-
-import com.sublinks.sublinksapi.api.lemmy.v3.utils.DateUtils;
 import com.sublinks.sublinksapi.community.dto.Community;
-import com.sublinks.sublinksapi.person.dto.LinkPersonCommunity;
 import com.sublinks.sublinksapi.person.dto.LinkPersonPost;
 import com.sublinks.sublinksapi.person.dto.Person;
 import com.sublinks.sublinksapi.person.enums.LinkPersonPostType;
 import com.sublinks.sublinksapi.person.enums.ListingType;
 import com.sublinks.sublinksapi.post.dto.Post;
-import com.sublinks.sublinksapi.post.dto.PostLike;
 import com.sublinks.sublinksapi.post.models.PostSearchCriteria;
+import com.sublinks.sublinksapi.post.services.PostSearchQueryBuilder;
+import com.sublinks.sublinksapi.post.services.PostSearchQueryBuilder.Builder;
+import com.sublinks.sublinksapi.post.services.PostSearchQueryBuilder.Results;
 import com.sublinks.sublinksapi.shared.RemovedState;
-import com.sublinks.sublinksapi.utils.CursorBasedPageable;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Expression;
@@ -24,105 +20,47 @@ import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.datetime.DateFormatter;
 
 @AllArgsConstructor
 public class PostRepositoryImpl implements PostRepositorySearch {
 
   private final EntityManager em;
+  private final PostSearchQueryBuilder postSearchQueryBuilder;
 
   @Override
   public List<Post> allPostsBySearchCriteria(final PostSearchCriteria postSearchCriteria) {
 
-    final CriteriaBuilder cb = em.getCriteriaBuilder();
-    final CriteriaQuery<Post> cq = cb.createQuery(Post.class);
-
-    final Root<Post> postTable = cq.from(Post.class);
-    final Join<Post, Community> communityJoin = postTable.join("community", JoinType.INNER);
-
-    final List<Predicate> predicates = new ArrayList<>();
-    // Community filter
-    if (postSearchCriteria.communityIds() != null && !postSearchCriteria.communityIds().isEmpty()) {
-      final Expression<Long> expression = communityJoin.get("id");
-      predicates.add(expression.in(postSearchCriteria.communityIds()));
+    final Builder searchBuilder = postSearchQueryBuilder.builder();
+    if (postSearchCriteria.person() != null) {
+      searchBuilder
+          .setPerson(postSearchCriteria.person())
+          .addPersonLikesToPost();
     }
-    // Subscribed only filter
+    if (postSearchCriteria.communityIds() != null) {
+      searchBuilder
+          .filterByCommunities(postSearchCriteria.communityIds());
+    }
     if (postSearchCriteria.person() != null
         && postSearchCriteria.listingType() == ListingType.Subscribed) {
-      final Join<Community, LinkPersonCommunity> linkPersonCommunityJoin = communityJoin.join(
-          "linkPersonCommunity", JoinType.INNER);
-      predicates.add(cb.equal(linkPersonCommunityJoin.get("person"), postSearchCriteria.person()));
+      searchBuilder
+          .filterByListingType(postSearchCriteria.listingType());
     }
-    // Join for PostLike
-    if (postSearchCriteria.person() != null) {
-      final Join<Post, PostLike> postPostLikeJoin = postTable.join("postLikes", JoinType.LEFT);
-      postPostLikeJoin.on(cb.equal(postPostLikeJoin.get("person"), postSearchCriteria.person()));
+    if (postSearchCriteria.cursorBasedPageable() != null) {
+      searchBuilder.setCursor(postSearchCriteria.cursorBasedPageable());
     }
-
-    CursorBasedPageable<Post> cursorBasedPageable = postSearchCriteria.cursorBasedPageable();
-
-    if (cursorBasedPageable != null) {
-      try {
-        final Map<String, String> values = cursorBasedPageable.getNextPageDecodedCursor();
-        final String primary = values.get("primary");
-        // 2024-03-17T11:07:39.000057+01 format
-
-        final Date secondary = new SimpleDateFormat(DateUtils.FRONT_END_DATE_FORMAT).parse(
-            values.get("secondary"));
-
-        final Predicate primaryPredicate = cb.lessThan(postTable.get("id"),
-            Integer.parseInt(primary));
-        final Predicate secondaryPredicate = switch (postSearchCriteria.sortType()) {
-          case New -> cb.lessThan(postTable.<Date>get("createdAt"), secondary);
-          case Old -> cb.greaterThan(postTable.<Date>get("createdAt"), secondary);
-          default -> cb.lessThan(postTable.<Date>get("createdAt"), secondary);
-        };
-        final Predicate secondaryPrimaryPredicate = cb.equal(postTable.get("createdAt"), primary);
-        predicates.add(
-            cb.or(cb.and(primaryPredicate, secondaryPrimaryPredicate), secondaryPredicate));
-      } catch (Exception e) {
-        System.out.println("Error in parsing date");
-        throw new IllegalArgumentException("Error in parsing Cursor");
-      }
+    Results results = postSearchQueryBuilder.results(searchBuilder);
+    if (postSearchCriteria.sortType() != null) {
+      results.setSortType(postSearchCriteria.sortType());
     }
-
-    cq.where(predicates.toArray(new Predicate[0]));
-
-    switch (postSearchCriteria.sortType()) {
-      case New:
-        cq.orderBy(cb.desc(postTable.get("createdAt")));
-        break;
-      case Old:
-        cq.orderBy(cb.asc(postTable.get("createdAt")));
-        break;
-      default:
-        cq.orderBy(cb.desc(postTable.get("createdAt")));
-        break;
+    results.setPerPage(postSearchCriteria.perPage());
+    if (postSearchCriteria.cursorBasedPageable() != null) {
+      return results.getCursorResults();
     }
-    int perPage = Math.min(Math.abs(postSearchCriteria.perPage()), 20);
-
-    TypedQuery<Post> query = em.createQuery(cq);
-    if (cursorBasedPageable == null) {
-      applyPagination(query, postSearchCriteria.page(), perPage);
-    } else {
-      applyPagination(query, null, perPage);
-    }
-
-    return query.getResultList();
+    results.setPage(1);
+    return results.getResults();
   }
 
   @Override
@@ -185,5 +123,4 @@ public class PostRepositoryImpl implements PostRepositorySearch {
 
     return em.createQuery(cq).getResultList();
   }
-
 }
--- src/main/java/com/sublinks/sublinksapi/search/services/SearchService.java
+++ src/main/java/com/sublinks/sublinksapi/search/services/SearchService.java
@@ -1,10 +1,10 @@
 package com.sublinks.sublinksapi.search.services;
 
-import com.sublinks.sublinksapi.comment.entities.Comment;
-import com.sublinks.sublinksapi.community.entities.Community;
-import com.sublinks.sublinksapi.person.entities.Person;
-import com.sublinks.sublinksapi.post.entities.CrossPost;
-import com.sublinks.sublinksapi.post.entities.Post;
+import com.sublinks.sublinksapi.comment.dto.Comment;
+import com.sublinks.sublinksapi.community.dto.Community;
+import com.sublinks.sublinksapi.person.dto.Person;
+import com.sublinks.sublinksapi.post.dto.CrossPost;
+import com.sublinks.sublinksapi.post.dto.Post;
 import com.sublinks.sublinksapi.post.repositories.CrossPostRepository;
 import com.sublinks.sublinksapi.post.services.PostService;
 import com.sublinks.sublinksapi.search.repositories.CommentSearchRepository;
@@ -24,52 +24,40 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class SearchService {
+    private final PostSearchRepository postSearchRepository;
+    private final CommentSearchRepository commentSearchRepository;
+    private final CommunitySearchRepository communitySearchRepository;
+    private final PersonSearchRepository personSearchRepository;
+    private final PostService postService;
+    private final CrossPostRepository crossPostRepository;
+    private final UrlUtil urlUtil;
 
-  private final PostSearchRepository postSearchRepository;
-  private final CommentSearchRepository commentSearchRepository;
-  private final CommunitySearchRepository communitySearchRepository;
-  private final PersonSearchRepository personSearchRepository;
-  private final PostService postService;
-  private final CrossPostRepository crossPostRepository;
-  private final UrlUtil urlUtil;
-
-  public Page<Community> searchCommunity(final String query, final int page, final int pageSize,
-      final Sort sort) {
-
-    return communitySearchRepository.searchAllByKeyword(query,
-        PageRequest.of(page, pageSize, sort));
-  }
-
-  public Page<Post> searchPost(final String query, final int page, final int pageSize,
-      final Sort sort) {
-
-    return postSearchRepository.searchAllByKeyword(query, PageRequest.of(page, pageSize, sort));
-  }
-
-  public Page<Comment> searchComments(final String query, final int page, final int pageSize,
-      final Sort sort) {
-
-    return commentSearchRepository.searchAllByKeyword(query, PageRequest.of(page, pageSize, sort));
-  }
-
-  public Page<Post> searchPostByUrl(final String url, final int page, final int pageSize,
-      final Sort sort) {
-
-    String cleanUrl = urlUtil.normalizeUrl(url);
-    String hash = postService.getStringMd5Hash(cleanUrl);
-    Optional<CrossPost> crossPost = crossPostRepository.getCrossPostByMd5Hash(hash);
-
-    if (crossPost.isEmpty()) {
-      return Page.empty();
+    public Page<Community> searchCommunity(final String query, final int page, final int pageSize, final Sort sort) {
+        return communitySearchRepository.searchAllByKeyword(query, PageRequest.of(page, pageSize, sort));
     }
-    List<Post> posts = crossPost.get().getPosts().stream().toList();
-    return new PageImpl<>(posts.subList(page * pageSize, (page + 1) * pageSize),
-        PageRequest.of(page, pageSize, sort), posts.size());
-  }
 
-  public Page<Person> seearchPerson(final String query, final int page, final int pageSize,
-      final Sort sort) {
+    public Page<Post> searchPost(final String query, final int page, final int pageSize, final Sort sort) {
+        return postSearchRepository.searchAllByKeyword(query, PageRequest.of(page, pageSize, sort));
+    }
 
-    return personSearchRepository.searchAllByKeyword(query, PageRequest.of(page, pageSize, sort));
-  }
+    public Page<Comment> searchComments(final String query, final int page, final int pageSize, final Sort sort) {
+        return commentSearchRepository.searchAllByKeyword(query, PageRequest.of(page, pageSize, sort));
+    }
+
+    public Page<Post> searchPostByUrl(final String url, final int page, final int pageSize, final Sort sort) {
+        String cleanUrl = urlUtil.normalizeUrl(url);
+        String hash = postService.getStringMd5Hash(cleanUrl);
+        Optional<CrossPost> crossPost = crossPostRepository.getCrossPostByMd5Hash(hash);
+
+        if(crossPost.isEmpty()){
+            return Page.empty();
+        }
+        List<Post> posts = crossPost.get().getPosts().stream().toList();
+        return new PageImpl<>(posts.subList(page * pageSize, (page + 1) * pageSize),
+                PageRequest.of(page, pageSize, sort), posts.size());
+    }
+
+    public Page<Person> searchPerson(final String query, final int page, final int pageSize, final Sort sort) {
+        return personSearchRepository.searchAllByKeyword(query, PageRequest.of(page, pageSize, sort));
+    }
 }
