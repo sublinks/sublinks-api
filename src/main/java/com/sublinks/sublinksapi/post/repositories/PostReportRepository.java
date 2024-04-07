4987f761efca5026781a47af89b445a42960660b
rooki <34943569+Pdzly@users.noreply.github.com>
2024-04-07 21:34:39 +0200

    Add report resolution when banning a user
    Added functionality where all reports related to a user's content are automatically marked as resolved when they are banned. This includes any reports associated with posts, comments, and private messages they have sent. The change simplifies administration by reducing the number of open reports when a problematic user is removed from the system.

--- src/main/java/com/sublinks/sublinksapi/post/repositories/PostReportRepository.java
+++ src/main/java/com/sublinks/sublinksapi/post/repositories/PostReportRepository.java
@@ -1,10 +1,20 @@
 package com.sublinks.sublinksapi.post.repositories;
 
 import com.sublinks.sublinksapi.community.dto.Community;
+import com.sublinks.sublinksapi.person.dto.Person;
+import com.sublinks.sublinksapi.post.dto.Post;
 import com.sublinks.sublinksapi.post.dto.PostReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface PostReportRepository extends JpaRepository<PostReport, Long>,
     PostReportRepositorySearch {
 
+
+  @Modifying
+  @Query("UPDATE PostReport pr SET pr.resolved = true, pr.resolver = :resolver WHERE pr.resolved = false AND pr.post = :post")
+  void resolveAllReportsByPost(@Param("post") Post post, @Param("resolver") Person resolver);
 }
