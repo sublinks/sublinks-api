93832c96eae809047f070b19a0d16622cb2d3cce
Devan <devandbenz@gmail.com>
2024-04-10 20:10:48 -0500

    update for entity folder restructure

--- src/main/java/com/sublinks/sublinksapi/federation/listeners/PersonActorCreateListener.java
+++ src/main/java/com/sublinks/sublinksapi/federation/listeners/PersonActorCreateListener.java
@@ -1,12 +1,10 @@
 package com.sublinks.sublinksapi.federation.listeners;
 
-import com.sublinks.sublinksapi.api.lemmy.v3.user.controllers.UserAuthController;
 import com.sublinks.sublinksapi.federation.enums.ActorType;
 import com.sublinks.sublinksapi.federation.enums.RoutingKey;
 import com.sublinks.sublinksapi.federation.models.Actor;
-import com.sublinks.sublinksapi.person.dto.Person;
+import com.sublinks.sublinksapi.person.entities.Person;
 import com.sublinks.sublinksapi.person.events.PersonCreatedEvent;
-import com.sublinks.sublinksapi.person.events.PersonUpdatedEvent;
 import com.sublinks.sublinksapi.queue.services.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
