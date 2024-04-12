0adf1a3d54b37de8a194f7bdf7c3d6f6d1dc0b97
Devan <devandbenz@gmail.com>
2024-04-12 10:22:50 -0500

    working on it

--- src/main/java/com/sublinks/sublinksapi/federation/listeners/CommunityActorCreateListener.java
+++ src/main/java/com/sublinks/sublinksapi/federation/listeners/CommunityActorCreateListener.java
@@ -5,22 +5,36 @@ import com.sublinks.sublinksapi.community.events.CommunityCreatedEvent;
 import com.sublinks.sublinksapi.federation.enums.ActorType;
 import com.sublinks.sublinksapi.federation.enums.RoutingKey;
 import com.sublinks.sublinksapi.federation.models.Actor;
+import com.sublinks.sublinksapi.queue.services.Producer;
+import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnBean(RabbitTemplate.class)
 public class CommunityActorCreateListener extends FederationListener implements
     ApplicationListener<CommunityCreatedEvent> {
 
   private static final Logger logger = LoggerFactory.getLogger(CommunityActorCreateListener.class);
 
+  @Autowired
+  public CommunityActorCreateListener(Producer federationProducer) {
+
+    super(federationProducer);
+    logger.info("community actor listener instantiated");
+  }
+
   @Override
-  public void onApplicationEvent(CommunityCreatedEvent event) {
+  public void onApplicationEvent(@NonNull CommunityCreatedEvent event) {
+
+    if (federationProducer == null) {
+      logger.error("federation producer is not instantiated properly");
+      return;
+    }
 
     Community community = event.getCommunity();
 
@@ -28,6 +42,7 @@ public class CommunityActorCreateListener extends FederationListener implements
         .actor_id(community.getActivityPubId())
         .actor_type(ActorType.COMMUNITY.getValue())
         .bio(community.getDescription())
+        .username(community.getTitleSlug())
         .display_name(community.getTitle())
         .private_key(community.getPrivateKey())
         .public_key(community.getPublicKey())
--- src/main/java/com/sublinks/sublinksapi/queue/services/Producer.java
+++ src/main/java/com/sublinks/sublinksapi/queue/services/Producer.java
@@ -8,14 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 @ConditionalOnBean(RabbitTemplate.class)
 public class Producer {
+
   private final RabbitTemplate rabbitTemplate;
 
   @Autowired
   public Producer(RabbitTemplate rabbitTemplate) {
+
     this.rabbitTemplate = rabbitTemplate;
   }
 
   public void sendMessage(String exchange, String routingKey, Object msg) {
+
     this.rabbitTemplate.convertAndSend(exchange, routingKey, msg);
   }
 }
