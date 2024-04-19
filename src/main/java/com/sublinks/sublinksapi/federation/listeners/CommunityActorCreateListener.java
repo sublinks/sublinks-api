4e91ef8bcab3264c49acd722a4f37388053bd15f
Jason Grim <jason@gr.im>
2024-04-19 18:04:29 -0400

    Fixes to bean initialization for federation queue

--- src/main/java/com/sublinks/sublinksapi/federation/listeners/CommunityActorCreateListener.java
+++ src/main/java/com/sublinks/sublinksapi/federation/listeners/CommunityActorCreateListener.java
@@ -5,13 +5,9 @@ import com.sublinks.sublinksapi.community.events.CommunityCreatedEvent;
 import com.sublinks.sublinksapi.federation.enums.ActorType;
 import com.sublinks.sublinksapi.federation.enums.RoutingKey;
 import com.sublinks.sublinksapi.federation.models.Actor;
-import com.sublinks.sublinksapi.queue.services.Producer;
 import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -20,13 +16,6 @@ public class CommunityActorCreateListener extends FederationListener implements
     ApplicationListener<CommunityCreatedEvent> {
 
   private static final Logger logger = LoggerFactory.getLogger(CommunityActorCreateListener.class);
-
-  @Autowired
-  public CommunityActorCreateListener(Producer federationProducer) {
-
-    super(federationProducer);
-    logger.info("community actor listener instantiated");
-  }
 
   @Override
   public void onApplicationEvent(@NonNull CommunityCreatedEvent event) {
