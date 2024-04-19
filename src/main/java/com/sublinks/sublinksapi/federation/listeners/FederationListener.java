758e89f709ce03d309e1b870c6f813788b32e10e
Jason Grim <jason@gr.im>
2024-04-19 18:04:29 -0400

    Fixes to bean initialization for federation queue

--- src/main/java/com/sublinks/sublinksapi/federation/listeners/FederationListener.java
+++ src/main/java/com/sublinks/sublinksapi/federation/listeners/FederationListener.java
@@ -1,21 +1,16 @@
 package com.sublinks.sublinksapi.federation.listeners;
 
 import com.sublinks.sublinksapi.queue.services.Producer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 
 @ConditionalOnBean(RabbitTemplate.class)
+@RequiredArgsConstructor
 public class FederationListener {
 
   @Value("${sublinks.federation.exchange}")
   public String federationExchange;
   public Producer federationProducer;
-
-  @Autowired
-  public FederationListener(Producer federationProducer) {
-
-    this.federationProducer = federationProducer;
-  }
 }
--- src/main/java/com/sublinks/sublinksapi/federation/listeners/PersonActorCreateListener.java
+++ src/main/java/com/sublinks/sublinksapi/federation/listeners/PersonActorCreateListener.java
@@ -5,13 +5,9 @@ import com.sublinks.sublinksapi.federation.enums.RoutingKey;
 import com.sublinks.sublinksapi.federation.models.Actor;
 import com.sublinks.sublinksapi.person.entities.Person;
 import com.sublinks.sublinksapi.person.events.PersonCreatedEvent;
-import com.sublinks.sublinksapi.queue.services.Producer;
 import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -20,13 +16,6 @@ public class PersonActorCreateListener extends FederationListener implements
     ApplicationListener<PersonCreatedEvent> {
 
   private static final Logger logger = LoggerFactory.getLogger(PersonActorCreateListener.class);
-
-  @Autowired
-  public PersonActorCreateListener(Producer federationProducer) {
-
-    super(federationProducer);
-    logger.info("person actor listener instantiated");
-  }
 
   @Override
   public void onApplicationEvent(@NonNull PersonCreatedEvent event) {
