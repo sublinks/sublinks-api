e92cb11c188eb1fa21a80332b4578183357568df
rooki <34943569+Pdzly@users.noreply.github.com>
2024-04-21 12:17:06 +0200

    Removed unused imports

--- src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/comment/models/VoteView.java
+++ src/main/java/com/sublinks/sublinksapi/api/lemmy/v3/comment/models/VoteView.java
@@ -2,7 +2,6 @@ package com.sublinks.sublinksapi.api.lemmy.v3.comment.models;
 
 import com.sublinks.sublinksapi.api.lemmy.v3.user.models.Person;
 import lombok.Builder;
-import java.util.List;
 
 @Builder
 public record VoteView(
--- src/main/java/com/sublinks/sublinksapi/post/repositories/PostRepository.java
+++ src/main/java/com/sublinks/sublinksapi/post/repositories/PostRepository.java
@@ -2,7 +2,6 @@ package com.sublinks.sublinksapi.post.repositories;
 
 import com.sublinks.sublinksapi.post.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositorySearch {
 
