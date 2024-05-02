111b82cffe7bfe9ddd98c8daabd11a302f5b147b
Jason Grim <jason@gr.im>
2024-05-02 15:00:46 -0400

    Added MediaService and RolePermissionMediaTypes, improved ImageController
    Created the structure for media management by adding the MediaService and MediaRepository classes. Also integrated a new enumeration RolePermissionMediaTypes to handle permissions related to media. Apart from this, enhanced the ImageController by better structuring the methods and adding more functionalities such as user authorization check and media persistence.

---
+++ src/main/java/com/sublinks/sublinksapi/media/entities/Media.java
@@ -0,0 +1,61 @@
+package com.sublinks.sublinksapi.media.entities;
+
+import com.sublinks.sublinksapi.person.entities.Person;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+/**
+ * Media class represents a media file associated with a Person.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "media")
+public class Media {
+
+  /**
+   * Relationships
+   */
+  @ManyToOne
+  @JoinColumn(name = "person_id")
+  private Person person;
+
+  /**
+   * Attributes
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(nullable = false, name = "delete_token")
+  private String deleteToken;
+
+  @Column(nullable = false)
+  private String file;
+
+  @CreationTimestamp
+  @Column(updatable = false, nullable = false, name = "created_at")
+  private Date createdAt;
+
+  @UpdateTimestamp
+  @Column(updatable = false, nullable = false, name = "updated_at")
+  private Date updatedAt;
+}
