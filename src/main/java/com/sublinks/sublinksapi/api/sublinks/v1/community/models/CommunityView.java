0d47e472c494a7fa404ce7ef0871f022351a74e3
rooki <34943569+Pdzly@users.noreply.github.com>
2024-05-08 21:58:32 +0200

    Add CommunityView and CommunityAggregates models
    New models, CommunityView and CommunityAggregates, were introduced to provide comprehensive information about communities. The SublinksCommunityController was also updated to use these models, thereby improving the details provided when fetching a community or list of communities.

---
+++ src/main/java/com/sublinks/sublinksapi/api/sublinks/v1/community/models/CommunityView.java
@@ -0,0 +1,12 @@
+package com.sublinks.sublinksapi.api.sublinks.v1.community.models;
+
+import com.sublinks.sublinksapi.api.sublinks.v1.person.models.PersonResponse;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record CommunityView(CommunityResponse communityResponse,
+                            CommunityAggregates communityAggregates,
+                            List<PersonResponse> moderators) {
+
+}
