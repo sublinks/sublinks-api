ce2cf18e20a427b1507bd020e827ff434642c3d1
rooki <34943569+Pdzly@users.noreply.github.com>
2024-05-13 08:11:36 +0200

    Add role expiration functionality
    This commit introduces the feature to set expiration for roles assigned to a user in both instance and community level. This has been implemented by adding an expiration date field to the role relationship in Person and LinkPersonCommunity entities. Also, a scheduled task is added to regularly check and clear the expired roles.

--- src/main/java/com/sublinks/sublinksapi/person/entities/Person.java
+++ src/main/java/com/sublinks/sublinksapi/person/entities/Person.java
@@ -65,11 +65,7 @@ public class Person implements UserDetails, Principal {
   Set<LinkPersonPost> linkPersonPost;
 
   @ManyToOne
-  @JoinTable(
-      name = "link_person_instances",
-      joinColumns = @JoinColumn(name = "person_id"),
-      inverseJoinColumns = @JoinColumn(name = "instance_id")
-  )
+  @JoinTable(name = "link_person_instances", joinColumns = @JoinColumn(name = "person_id"), inverseJoinColumns = @JoinColumn(name = "instance_id"))
   private Instance instance;
 
   @ManyToOne(fetch = FetchType.EAGER)
@@ -171,7 +167,6 @@ public class Person implements UserDetails, Principal {
 
   @Column(nullable = false, name = "default_listing_type")
   @Enumerated(EnumType.STRING)
-
   private ListingType defaultListingType;
 
   @Column(nullable = false, name = "default_sort_type")
@@ -181,7 +176,6 @@ public class Person implements UserDetails, Principal {
 
   @Column(nullable = false, name = "post_listing_type")
   @Enumerated(EnumType.STRING)
-
   private PostListingMode postListingType;
 
   @Column(nullable = false, name = "is_infinite_scroll")
@@ -238,6 +232,9 @@ public class Person implements UserDetails, Principal {
   @Column(nullable = true, name = "totp_verified_secret")
   private String totpVerifiedSecret;
 
+  @Column(nullable = true, name = "role_expire_at")
+  private Date roleExpireAt;
+
   @CreationTimestamp
   @Column(updatable = false, nullable = false, name = "created_at")
   private Date createdAt;
@@ -253,6 +250,11 @@ public class Person implements UserDetails, Principal {
     }
 
     return RolePermissionService.isBanned(getRole());
+  }
+
+  public boolean isRoleExpired() {
+
+    return roleExpireAt != null && roleExpireAt.before(new Date());
   }
 
   public boolean isAdmin() {
