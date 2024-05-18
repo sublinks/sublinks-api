79cc25084606d870240ab3f5b59290b2ef401e71
rooki <34943569+Pdzly@users.noreply.github.com>
2024-05-18 12:26:48 +0200

    Refactor person setting update logic in UserController
    The logic of updating multiple fields of the Person object in the UserController has been refactored. The original combined setters and condition checks were broken down into individual condition check and setter lines. This increases readability and ensures that each field is properly checked before assignment.
    
    Fixed editorconfig

--- src/main/java/com/sublinks/sublinksapi/language/repositories/LanguageRepository.java
+++ src/main/java/com/sublinks/sublinksapi/language/repositories/LanguageRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LanguageRepository extends JpaRepository<Language, Long> {
 
   Language findLanguageByCode(String code);
+
 }
