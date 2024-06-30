e99b1459ebf6cc8ae263f5114749e7a7d7e3339c
rooki <rooki@rooki.xyz>
2024-06-30 15:04:31 +0200

    Refactor SublinksPersonMetaDataMapper class
    The commit removes unused methods and imports from the SublinksPersonMetaDataMapper class. The 'updatedAt' mapping source has also been changed from 'metadata.updatedAt' to 'metadata.lastUsedAt'. Besides, the 'mapPersonKey' method and associated Person import have been removed since they are no longer being used.
    
    Signed-off-by: rooki <rooki@rooki.xyz>

--- src/main/java/com/sublinks/sublinksapi/api/sublinks/v1/person/mappers/SublinksPersonMetaDataMapper.java
+++ src/main/java/com/sublinks/sublinksapi/api/sublinks/v1/person/mappers/SublinksPersonMetaDataMapper.java
@@ -3,12 +3,10 @@ package com.sublinks.sublinksapi.api.sublinks.v1.person.mappers;
 import com.sublinks.sublinksapi.api.sublinks.v1.person.models.PersonSessionData;
 import com.sublinks.sublinksapi.api.sublinks.v1.roles.mappers.SublinksRoleMapper;
 import com.sublinks.sublinksapi.api.sublinks.v1.utils.DateUtils;
-import com.sublinks.sublinksapi.person.entities.Person;
 import com.sublinks.sublinksapi.person.entities.PersonMetaData;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.Named;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
 
@@ -25,15 +23,7 @@ public abstract class SublinksPersonMetaDataMapper implements
       source = "metadata.createdAt",
       dateFormat = DateUtils.FRONT_END_DATE_FORMAT)
   @Mapping(target = "updatedAt",
-      source = "metadata.updatedAt",
+      source = "metadata.lastUsedAt",
       dateFormat = DateUtils.FRONT_END_DATE_FORMAT)
-  public abstract PersonSessionData convert(
-      @Nullable PersonMetaData metadata);
-
-  @Named("personKey")
-  String mapPersonKey(Person person) {
-
-    return person.getName() + "@" + person.getInstance()
-        .getDomain();
-  }
+  public abstract PersonSessionData convert(@Nullable PersonMetaData metadata);
 }
